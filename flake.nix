0e8d1c3f12b3d79046dad8724f95e06e5bf7d7f2
emanuel <134736553+EmanuelM153@users.noreply.github.com>
2024-09-19 18:52:50 -0500

    Version with necesarry tools for downloading ns-3

--- flake.nix
+++ flake.nix
@@ -17,14 +17,37 @@
           # (python.withPackages (ps: [
           #   ps.cppyy-cling
           # ]))
+          # base
+          git
+          unzip
+          gnumake
+          gnupatch
+          gcc
+          gnutar
+          python312
+          python312Packages.pip
+          cmake
+
+          # required by bake
           python312Packages.distro
           python312Packages.requests
-          cmake
           automake
+
+          # requirede by ns-3
+          python312Packages.mercurial
+
+          # python312Packages.pyqt5
+          # libsForQt5.full
+          # libsForQt5.qt5.qtbase
+          # qtcreator
+          # for pygobject
+          # cairo
+          # gobject-introspection
+          # gobject-introspection-unwrapped
         ];
 
         shellHook = ''
-          For installing pip packages as discussed in 
+          # For installing pip packages as discussed in 
           export PIP_PREFIX=$(pwd)/_build/pip_packages
           export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
           export PATH="$PIP_PREFIX/bin:$PATH"
